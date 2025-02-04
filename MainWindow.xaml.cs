04c1b07dc2f2265eb3d1700a45baae928d14f8ac
agus-marmor <amarmor06@gmail.com>
2025-02-03 20:54:01 -0500

    Added Save and Load Buttons, Design is not done yet

--- MainWindow.xaml.cs
+++ MainWindow.xaml.cs
@@ -1,5 +1,7 @@
 ﻿
-using System.Reflection;
+using System.ComponentModel;
+using System.IO;
+using System.Text.Json;
 using System.Windows;
 using System.Windows.Controls;
 using System.Windows.Input;
@@ -19,9 +21,50 @@ namespace ToDoListApp
             //Set the image source for app icon
             Uri iconUri = new Uri("pack://application:,,,/Assets/app-icon.png");
             Icon = BitmapFrame.Create(iconUri);
-            // Add
-           
+            this.Closing += MainWindow_Closing;
 
+        }
+
+        private void MainWindow_Closing(object? sender, CancelEventArgs e)
+        {
+            SaveTasks("automatic");
+        }
+
+        private void SaveTasksButton_Click(object sender, RoutedEventArgs e)
+        {
+          SaveTasks("manual");
+        }
+
+        private void SaveTasks(string type)
+        {
+            List<string> tasks = new List<string>();
+            foreach (var item in TaskList.Items)
+            {
+                tasks.Add(item.ToString());
+            }
+
+            string json = JsonSerializer.Serialize(tasks);
+
+            string path = "tasks.json";
+            File.WriteAllText(path, json);
+            if(type == "manual")
+                MessageBox.Show("Tasks saved successfully!", "Save Tasks", MessageBoxButton.OK, MessageBoxImage.Information);
+        }
+
+        private void LoadTasks()
+        {
+            string path = "tasks.json";
+            if (File.Exists(path))
+            {
+                string json = File.ReadAllText(path);
+                List<string> tasks = JsonSerializer.Deserialize<List<string>>(json); 
+                File.Delete(path);
+                TaskList.Items.Clear();
+                foreach (var task in tasks)
+                {
+                    TaskList.Items.Add(task);
+                }
+            }
         }
 
         private void AddTaskButton_Click(object sender, RoutedEventArgs e)
@@ -44,6 +87,11 @@ namespace ToDoListApp
                 TaskList.Items.Remove(task);
             }
 
+        }
+
+        private void LoadTasksButton_Click(object sender, RoutedEventArgs e)
+        {
+            LoadTasks();
         }
 
         private void TaskInput_KeyDown(object sender, KeyEventArgs e)
